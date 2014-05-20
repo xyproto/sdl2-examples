7fbd1e3f63ded4c9ee243978f03bc27adb810247
Alexander Rødseth <rodseth@gmail.com>
2014-05-20 16:28:36 +0200

    Reorganized

---
+++ c++11/main.cpp
@@ -0,0 +1,47 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+  if (win == nullptr) {
+    std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (ren == nullptr) {
+    std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_Surface *bmp = SDL_LoadBMP("../data/test.bmp");
+  if (bmp == nullptr) {
+    std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+  SDL_FreeSurface(bmp);
+  if (tex == nullptr) {
+    std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_RenderClear(ren);
+  SDL_RenderCopy(ren, tex, NULL, NULL);
+  SDL_RenderPresent(ren);
+
+  SDL_Delay(2000);
+
+  SDL_DestroyTexture(tex);
+  SDL_DestroyRenderer(ren);
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+
+  return 0;
+}
