8b54f12960c08c7043bc15cdfadf6e954efac48b
Alexander F Rødseth <rodseth@gmail.com>
2014-05-26 17:37:57 +0200

    Minor changes

--- go/main.go
+++ go/main.go
@@ -5,25 +5,28 @@ import (
 	"log"
 )
 
-// This file is more similar to the C and C++ examples than strictly needed
-
 func main() {
 	// Initialize
 	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
 		log.Fatalf("SDL_Init Error: %s\n", sdl.GetError())
 	}
+	// Make sure to quit when the function returns
 	defer sdl.Quit()
 
 	// Prepare the window and load the image
 	win := sdl.CreateWindow("Hello World!", 100, 100, 960, 540, sdl.WINDOW_SHOWN)
 	defer win.Destroy()
+
+	// Prepare a renderer for the window
 	ren := sdl.CreateRenderer(win, -1, sdl.RENDERER_ACCELERATED|sdl.RENDERER_PRESENTVSYNC)
 	defer ren.Destroy()
+
+	// Load the image and prepare to use it as a texture
 	bmp := sdl.LoadBMP("../img/boxes.bmp")
 	tex := ren.CreateTextureFromSurface(bmp)
 	defer tex.Destroy()
 
-	// Show the image
+	// Show the texture
 	ren.Clear()
 	ren.Copy(tex, nil, nil)
 	ren.Present()
