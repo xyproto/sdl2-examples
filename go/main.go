5d5b699aea82f4de485e7a6a73150f713c183c6f
Alexander Rødseth <rodseth@gmail.com>
2014-05-28 12:21:30 +0200

    Better error handling

--- go/main.go
+++ go/main.go
@@ -5,25 +5,43 @@ import (
 	"log"
 )
 
+// Note that the SDL2 package could have been more ideomatic by
+// returning error values that could contain error messages instead
+// of having to use "if == nil", "sdl.GetError()" and the .Destroy() functions.
+
 func main() {
 	// Initialize
 	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
-		log.Fatalf("SDL_Init Error: %s\n", sdl.GetError())
+		log.Fatalf("Init Error: %s\n", sdl.GetError())
 	}
 	// Make sure to quit when the function returns
 	defer sdl.Quit()
 
-	// Prepare the window and load the image
+	// Create the window
 	win := sdl.CreateWindow("Hello World!", 100, 100, 960, 540, sdl.WINDOW_SHOWN)
+	if win == nil {
+		log.Fatalf("CreateWindow Error: %s\n", sdl.GetError())
+	}
 	defer win.Destroy()
 
-	// Prepare a renderer for the window
+	// Create a renderer
 	ren := sdl.CreateRenderer(win, -1, sdl.RENDERER_ACCELERATED|sdl.RENDERER_PRESENTVSYNC)
+	if win == nil {
+		log.Fatalf("CreateRenderer Error: %s\n", sdl.GetError())
+	}
 	defer ren.Destroy()
 
-	// Load the image and prepare to use it as a texture
+	// Load the image
 	bmp := sdl.LoadBMP("../img/boxes.bmp")
+	if bmp == nil {
+		log.Fatalf("LoadBMP Error: %s\n", sdl.GetError())
+	}
+
+	// Use the image as a texture
 	tex := ren.CreateTextureFromSurface(bmp)
+	if tex == nil {
+		log.Fatalf("CreateTextureFromSurface Error: %s\n", sdl.GetError())
+	}
 	defer tex.Destroy()
 
 	// Show the texture
