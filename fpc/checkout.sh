fef187a97a8d5f7844048d42671d1f6c6ffa4fe8
Alexander Rødseth <rodseth@gmail.com>
2015-04-22 11:39:10 +0200

    The repo for SDL2 for Free Pascal moved

--- fpc/checkout.sh
+++ fpc/checkout.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf SDL2
-git clone https://bitbucket.org/p_daniel/sdl-2-for-free-pascal-compiler.git SDL2
+git clone https://github.com/danpla/sdl2-fpc SDL2
 mv SDL2/SDL2 temp
 rm -rf SDL2
 mv temp SDL2
