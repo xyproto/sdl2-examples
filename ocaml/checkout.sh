7bae0ba5906f1864b42bf89030d45595bee2a39c
Alexander F Rødseth <rodseth@gmail.com>
2015-11-14 22:33:30 +0100

    Make OCamlSDL2 compile with OCaml 4.02.3

--- ocaml/checkout.sh
+++ ocaml/checkout.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 [[ -d OCamlSDL2 ]] || git clone -q https://github.com/fccm/OCamlSDL2
 cp OCamlSDL2/src/Makefile.config.unix OCamlSDL2/src/Makefile.config
+sed 's/\*\*/*/g' -i OCamlSDL2/src/sdlpower.ml
