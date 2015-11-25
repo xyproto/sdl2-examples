554933489c872878af3d0363edf8535285bb542f
Alexander F Rødseth <rodseth@gmail.com>
2015-11-25 15:59:03 +0100

    Add working example for Rust

---
+++ rust/src/main.rs
@@ -0,0 +1,48 @@
+extern crate sdl2;
+
+use std::path::Path;
+use sdl2::surface::{Surface};
+
+fn main() {
+    let ctx = sdl2::init().unwrap();
+    let video_ctx = ctx.video().unwrap();
+    let mut timer = ctx.timer().unwrap();
+
+    // Create a new OpenGL window
+    let window  = match video_ctx.window("Hello World!", 960, 540).position_centered().opengl().build() {
+        Ok(window) => window,
+        Err(err)   => panic!("failed to create window: {}", err)
+    };
+
+    // Load the image as a surface
+    let surface = match Surface::load_bmp(&Path::new("../img/boxes.bmp")) {
+        Ok(surface) => surface,
+        Err(err)    => panic!("failed to load surface: {}", err)
+    };
+
+    {
+        {
+            // Get the window surface
+            let events = ctx.event_pump().unwrap();
+            let screen = window.surface(&events).unwrap();
+
+            // Blit the image to the window
+            unsafe {
+                let _ = surface.blit(None, Surface::from_ll(screen.raw()), None);
+            }
+        }
+
+        for _ in 0..20 {
+
+            // Update the window to display the changed surface
+            match window.update_surface() {
+                Ok(_) => {},
+                Err(err) => panic!("failed to update window surface: {}", err)
+            }
+
+            // Wait 100ms
+            timer.delay(100);
+        }
+    }
+
+}
