a9f13cf8672404a103828a8a9db0a8442cb99e2d
Alexander F Rødseth <rodseth@gmail.com>
2017-04-03 13:43:44 +0200

    Update the git clone URL

--- fpc/checkout.sh
+++ fpc/checkout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-[[ -d SDL2 ]] || git clone -q https://github.com/danpla/sdl2-fpc SDL2
+[[ -d SDL2 ]] || git clone -q https://git@github.com/de-signer/sdl2-fpc SDL2
 [[ -d SDL2/SDL2 ]] && (mv SDL2/SDL2 temp; rm -rf SDL2; mv temp SDL2; chmod -x SDL2/*)
 true
