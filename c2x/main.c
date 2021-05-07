c009edb19ca8ca885f30a4159d63da51c01a884a
Alexander F. Rødseth <rodseth@gmail.com>
2021-05-07 09:42:35 +0200

    Only check for NULL once before destroying

--- c2x/main.c
+++ c2x/main.c
@@ -9,57 +9,45 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
+	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
 	if (win == NULL) {
 		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
 		return EXIT_FAILURE;
 	}
 
-	SDL_Renderer *ren = SDL_CreateRenderer(win, -1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == NULL) {
 		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
-		if (win != NULL) {
-			SDL_DestroyWindow(win);
-		}
+		SDL_DestroyWindow(win);
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
 
-	SDL_Surface *bmp = SDL_LoadBMP("../img/grumpy-cat.bmp");
+	SDL_Surface* bmp = SDL_LoadBMP("../img/grumpy-cat.bmp");
 	if (bmp == NULL) {
 		fprintf(stderr, "SDL_LoadBMP Error: %s\n", SDL_GetError());
-		if (ren != NULL) {
-			SDL_DestroyRenderer(ren);
-		}
-		if (win != NULL) {
-			SDL_DestroyWindow(win);
-		}
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
 
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
 	if (tex == NULL) {
 		fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
-		if (bmp != NULL) {
-			SDL_FreeSurface(bmp);
-		}
-		if (ren != NULL) {
-			SDL_DestroyRenderer(ren);
-		}
-		if (win != NULL) {
-			SDL_DestroyWindow(win);
-		}
+		SDL_FreeSurface(bmp);
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
 	SDL_FreeSurface(bmp);
 
-	for (int i=0; i < 20; i++) {
-			SDL_RenderClear(ren);
-			SDL_RenderCopy(ren, tex, NULL, NULL);
-			SDL_RenderPresent(ren);
-			SDL_Delay(100);
+	for (int i = 0; i < 20; i++) {
+		SDL_RenderClear(ren);
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		SDL_RenderPresent(ren);
+		SDL_Delay(100);
 	}
 
 	SDL_DestroyTexture(tex);
