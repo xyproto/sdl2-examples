8cdd02db1956d067223f4fba1dd8018db7e312af
Alexander F. Rødseth <rodseth@gmail.com>
2021-08-02 18:53:12 +0200

    Update the Zig example to Zig 0.8

--- zig/main.zig
+++ zig/main.zig
@@ -5,7 +5,7 @@ pub fn main() !u8 {
 
     // Initialize SDL2
     if (sdl2.SDL_Init(sdl2.SDL_INIT_EVERYTHING) != 0) {
-        print("SDL_Init Error: {}\n", .{sdl2.SDL_GetError()});
+        print("SDL_Init Error: {s}\n", .{sdl2.SDL_GetError()});
         return 1;
     }
     defer sdl2.SDL_Quit();
@@ -13,7 +13,7 @@ pub fn main() !u8 {
     // Create a SDL_Window
     var win: ?*sdl2.SDL_Window = sdl2.SDL_CreateWindow("Hello World!", 100, 100, 620, 387, sdl2.SDL_WINDOW_SHOWN);
     if (win == null) {
-        print("SDL_CreateWindow Error: {}\n", .{sdl2.SDL_GetError()});
+        print("SDL_CreateWindow Error: {s}\n", .{sdl2.SDL_GetError()});
         return 1;
     }
     defer sdl2.SDL_DestroyWindow(win);
@@ -21,7 +21,7 @@ pub fn main() !u8 {
     // Create a SDL_Renderer
     var ren: ?*sdl2.SDL_Renderer = sdl2.SDL_CreateRenderer(win, -1, sdl2.SDL_RENDERER_ACCELERATED | sdl2.SDL_RENDERER_PRESENTVSYNC);
     if (ren == null) {
-        print("SDL_CreateRenderer Error: {}\n", .{sdl2.SDL_GetError()});
+        print("SDL_CreateRenderer Error: {s}\n", .{sdl2.SDL_GetError()});
         return 1;
     }
     defer sdl2.SDL_DestroyRenderer(ren);
@@ -29,7 +29,7 @@ pub fn main() !u8 {
     // Load the image as an SDL_Surface
     var bmp: ?*sdl2.SDL_Surface = sdl2.SDL_LoadBMP("../img/grumpy-cat.bmp");
     if (bmp == null) {
-        print("SDL_LoadBMP Error: {}\n", .{sdl2.SDL_GetError()});
+        print("SDL_LoadBMP Error: {s}\n", .{sdl2.SDL_GetError()});
         return 1;
     }
     defer sdl2.SDL_FreeSurface(bmp);
@@ -37,7 +37,7 @@ pub fn main() !u8 {
     // Create a SDL_Texture from the SDL_Surface
     var tex: ?*sdl2.SDL_Texture = sdl2.SDL_CreateTextureFromSurface(ren, bmp);
     if (tex == null) {
-        print("SDL_CreateTextureFromSurface Error: {}\n", .{sdl2.SDL_GetError()});
+        print("SDL_CreateTextureFromSurface Error: {s}\n", .{sdl2.SDL_GetError()});
         return 1;
     }
     defer sdl2.SDL_DestroyTexture(tex);
