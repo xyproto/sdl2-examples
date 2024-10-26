80f6529364d57394b9cf40d097604d6ea37f4b8b
Sijawusz Pur Rahnama <sija@sija.pl>
2024-10-26 23:17:33 +0200

    Make Crystal example a bit more Crystal-ish (#40)

--- crystal/src/main.cr
+++ crystal/src/main.cr
@@ -3,7 +3,6 @@ require "sdl/image"
 
 begin
   SDL.init(SDL::Init::VIDEO)
-
   at_exit { SDL.quit }
 
   window = SDL::Window.new("Hello, World!", 620, 387)
@@ -14,17 +13,16 @@ begin
   png = SDL::IMG.load(File.join("..", "img", "grumpy-cat.png"))
   png = png.convert(window.surface)
 
-  start_time = Time.utc
+  start_time = Time.monotonic
   loop do
     png.blit(window.surface)
     window.update
 
-    if (Time.utc - start_time).seconds > 2
+    if (Time.monotonic - start_time).total_seconds > 2
       break
     end
   end
 
 rescue ex
-  puts "Error: " + ex.message.to_s
-  exit 1
+  abort "Error: #{ex.message}"
 end
