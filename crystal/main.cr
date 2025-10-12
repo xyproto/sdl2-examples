79e094fa5dbdad369e9de125ebbf4481fc80f82e
Sijawusz Pur Rahnama <sija@sija.pl>
2025-10-12 20:31:03 +0200

    Make Crystal example more idiomatic

--- crystal/main.cr
+++ crystal/main.cr
@@ -2,38 +2,36 @@ require "sdl"
 require "sdl/image"
 
 begin
- SDL.init(SDL::Init::VIDEO)
- at_exit { SDL.quit }
+  SDL.init(SDL::Init::VIDEO)
+  at_exit { SDL.quit }
 
- SDL::IMG.init(SDL::IMG::Init::PNG)
- at_exit { SDL::IMG.quit }
+  SDL::IMG.init(SDL::IMG::Init::PNG)
+  at_exit { SDL::IMG.quit }
 
- window = SDL::Window.new("Hello World!", 620, 387)
- renderer = SDL::Renderer.new(window, SDL::Renderer::Flags::ACCELERATED | SDL::Renderer::Flags::PRESENTVSYNC)
+  window = SDL::Window.new("Hello World!", 620, 387)
+  renderer = SDL::Renderer.new(window, SDL::Renderer::Flags::ACCELERATED | SDL::Renderer::Flags::PRESENTVSYNC)
 
- png = SDL::IMG.load(File.join("..", "img", "grumpy-cat.png"), renderer)
- texture = png
+  texture = SDL::IMG.load(File.join(__DIR__, "..", "img", "grumpy-cat.png"), renderer)
 
- quit = false
- start_time = Time.monotonic.total_milliseconds.to_i
+  start_time = Time.monotonic
+  quit = false
 
- while !quit
-   case event = SDL::Event.poll
-   when SDL::Event::Quit
-     quit = true
+  until quit
+    case event = SDL::Event.poll
+    when SDL::Event::Quit
+      quit = true
     when SDL::Event::Keyboard
       quit = true if event.keydown? && event.sym.escape?
-   end
+    end
 
-   elapsed_time = Time.monotonic.total_milliseconds.to_i - start_time
-   break if elapsed_time > 2000
+    break if (Time.monotonic - start_time) > 2.seconds
 
-   renderer.clear
-   renderer.copy(texture)
-   renderer.present
-   sleep(100.milliseconds)
- end
+    renderer.clear
+    renderer.copy(texture)
+    renderer.present
 
+    sleep(100.milliseconds)
+  end
 rescue ex
- abort "Error: #{ex.message}"
+  abort "Error: #{ex.message}"
 end
